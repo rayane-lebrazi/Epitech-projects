c966074cc4e9f55c32604c6a27fde2bc1514758b
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-24 10:16:38 +0200

    task1.4

---
+++ day04/task1.4/main.py
@@ -0,0 +1,10 @@
+string = input("Give me a string: ")
+
+if string == "open sesame":
+    print("access granted")
+
+elif string == "will you open, you goddamn !@&/°":
+    print("access fucking granted")
+
+else:
+    print("permission denied")
