6116c414a996dd8b70c8a39098a9e10141ed7381
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-24 10:25:08 +0200

    task1.5

--- day04/task1.5/main.py
+++ day04/task1.5/main.py
@@ -1,8 +1,19 @@
-number = int(input("Give me an integer: \n"))
+number = int(input("Give me an integer: "))
 
-if number == 42 :
-    print("a")
-elif number <= 21 :
-    print("b")
-elif number % 2 == 0:
-    print("c")
+if number == 42:
+    print("a", end="")
+
+if number <= 21:
+    print("b", end="")
+
+if number % 2 == 0:
+    print("c", end="")
+
+if number / 2 < 21:
+    print("d", end="")
+
+if number % 2 != 0 and number >= 45:
+    print("e", end="")
+
+if not (number == 42 or number <= 21 or number % 2 == 0 or number / 2 < 21 or (number % 2 != 0 and number >= 45)):
+    print("f", end="")
