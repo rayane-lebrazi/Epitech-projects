f0cf784b4b5258ef97498f72fa42d222f505206d
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-25 11:51:41 +0200

    task2.5

---
+++ day05/task2.5/main.py
@@ -0,0 +1,12 @@
+types = {
+    "Electric": [],
+    "Grass": [],
+    "Fire": []
+}
+types["Electric"].append("Pikachu")
+types["Grass"].append("Bulbasaur")
+types["Fire"].append("Charmander")
+types["Grass"].append("Leafeon")
+types["Fire"].append("Scovillain")
+
+print(types)
