6777adec3384a091afb5c00e3c4719163f583855
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-25 12:02:01 +0200

    task2.7

---
+++ day05/task2.7/main.py
@@ -0,0 +1,8 @@
+pokemons = {
+    "Pikachu": ["Electric"],
+    "Grass": ["Bulbasaur", "Leafeon"],
+    "Fire": ["Charmander", "Scovillain"]
+}
+print(pokemons["Pikachu"])
+print("the second way:")
+print(pokemons.get("Pikachu"))
