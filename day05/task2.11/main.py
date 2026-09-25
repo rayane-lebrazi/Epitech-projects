7ccfb777e5cca9a5c8d95819e37b944aae7c8406
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-25 14:04:51 +0200

    task2.11

---
+++ day05/task2.11/main.py
@@ -0,0 +1,12 @@
+dict = {
+"dalmatians": 101,
+"pi": 3.14,
+"beast": 666,
+"life": 42,
+"googol": 10**100,
+"jordan": 23,
+"life, the universe and everything": 42,
+"emergency": 911,
+"euler": 2.71828}
+
+print(max(dict, key=dict.get))
