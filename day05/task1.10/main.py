b2642585bc44f4f93fb2223fe6763df506084e95
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-25 10:37:48 +0200

    task1.10

---
+++ day05/task1.10/main.py
@@ -0,0 +1,7 @@
+my_first_list = [4, 5, 6]
+my_second_list = [1, 2, 3]
+my_first_list.extend(my_second_list)
+print(my_first_list)
+
+#this code adds two lists to each other, the output is the following: 
+#[4, 5, 6, 1, 2, 3]
