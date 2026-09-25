c367764637f82d1becb22cec81039f0469db45cf
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-25 11:46:16 +0200

    task2.3

---
+++ day05/task2.3/main.py
@@ -0,0 +1,10 @@
+pokemons = {
+    "Pikachu": "Electric",
+    "Bulbasaur": "Grass",
+    "Charmander": "Fire"
+}
+
+pokemons["Pikachu"] = ["Pichu", "Raichu"]
+
+print(pokemons)
+#We can say that a dictionary can hold multiple values including lists
