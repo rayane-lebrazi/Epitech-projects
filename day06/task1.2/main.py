1e02185257e1537fadc103770b45548e60d11fa8
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-26 13:01:39 +0200

    task1.2

---
+++ day06/task1.2/main.py
@@ -0,0 +1,19 @@
+def bread():
+    print("<//////////>")
+
+def lettuce():
+    print("~~~~~~~~~~~~")
+
+def tomato():
+    print("O O O O O O")
+
+def ham():
+    print("============")
+
+
+bread()
+lettuce()
+tomato()
+ham()
+ham()
+bread()
