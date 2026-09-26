7b82bce380587e7b2a2b85b5aff811ee5f5f2dd7
Rayane Lebrazi <rayane.lebrazi@epitech.eu>
2026-09-26 12:53:28 +0200

    task1.1

--- day05/task1.2/main.py
+++ day05/task1.2/main.py
@@ -1,2 +1,2 @@
 list = [1, 2, 3, 4, 5, 6]
-print("The las element of you list is: ", list[len(list)-1])
+print("The las element of you list is: ", list[-1])
---
+++ day06/task1.1/main.py
@@ -0,0 +1,10 @@
+def f1():
+    return 42
+
+
+def f2(x):
+    return 2 * x
+
+
+print(f1(), f2(5) + f1())
+#the difference between these two function is one takes a variabel as a parametre and does operrations on it and the otehrone doesnt take anything as parametre and returns it straight away
